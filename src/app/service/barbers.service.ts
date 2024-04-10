e0e0bbab2880f5a8300cd8144ae976e01005a160
erikhernandez9 <erik.hernandez@correo.ucu.edu.uy>
2024-04-10 15:29:50 -0300

    service booking

--- src/app/service/barbers.service.ts
+++ src/app/service/barbers.service.ts
@@ -1,28 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Barber } from '../types/barberInterface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BarbersService {
 
-  barbers = [
+  barbers : Barber[] = [
     {
       id: 1,
       name: 'Juan',
       cel: '09999999',
       age: 25,
+      booking : {
+        cancelled: [],
+        finished: [],
+        pending: []
+      }
     },
     {
       id: 2,
       name: 'Pedro',
       cel: '09999999',
       age: 25,
+      booking : {
+        cancelled: [],
+        finished: [],
+        pending: []
+      }
     },
     {
       id: 3,
       name: 'Maria',
       cel: '09999999',
       age: 25,
+      booking : {
+        cancelled: [],
+        finished: [],
+        pending: []
+      }
     },
   ];
 
@@ -30,5 +46,9 @@ export class BarbersService {
 
   getBarbers() {
     return this.barbers;
+  }
+
+  getBarber(id: number) {
+    return this.barbers.find(barber => barber.id === id);
   }
 }
--- src/app/service/booking.service.ts
+++ src/app/service/booking.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@angular/core';
+import { BarbersService } from './barbers.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingService {
 
-  
 
-  constructor() { }
+
+  constructor(private barberService: BarbersService) { }
+
+  cancelBooking(bookingId: number, barberId: number, description: string) {
+    const barber = this.barberService.getBarber(barberId);
+    if (barber) {
+      const booking = barber.booking.pending.find(booking => booking.id === bookingId);
+      if (booking) {
+        booking.status = 'cancelled';
+        booking.description = description;
+        barber.booking.cancelled.push(booking);
+        barber.booking.pending = barber.booking.pending.filter(booking => booking.id !== bookingId);
+      }
+    }
+  }
 
 
 }
