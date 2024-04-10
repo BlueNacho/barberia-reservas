f7574438814f97f43ac72a3120ba1f01eb21209e
erikhernandez9 <erik.hernandez@correo.ucu.edu.uy>
2024-04-10 15:22:57 -0300

    type barber

--- src/app/cancel-booking/cancel-booking.component.ts
+++ src/app/cancel-booking/cancel-booking.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { bookingInterface } from '../types/bookingInterface';
 
 @Component({
   selector: 'app-cancel-booking',
@@ -6,11 +7,22 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./cancel-booking.component.scss']
 })
 export class CancelBookingComponent {
-  @Input() booking: any;
+  @Input() booking: bookingInterface = {
+    id: 0,
+    date: '',
+    client: '',
+    service: '',
+    barber: '',
+    status: '',
+  }
+
+  description: string = '';
+
   constructor() { }
 
   cancelBooking() {
-    
+    this.booking.status = 'cancelled';
+    this.booking.description = this.description;
     console.log('Cancel booking');
   }
 }
--- src/app/types/barberInterface.ts
+++ src/app/types/barberInterface.ts
@@ -1,6 +1,13 @@
+import { bookingInterface } from "./bookingInterface";
+
 export interface Barber {
     id: number;
     name: string;
     cel: string;
     age: number;
+    booking : {
+        cancelled: bookingInterface[],
+        finished: bookingInterface[],
+        pending: bookingInterface[]
+    }
 }
